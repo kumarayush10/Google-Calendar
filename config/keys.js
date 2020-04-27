db20575606bd87b092bbedc4eef0dbaf023bd3c2
brilliant-ayush <ayush.kumar80g@gmail.com>
2020-04-27 14:05:53 +0530

    Delete keys.js

--- config/keys.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    google: {
-        clientID: '196759350850-ar12ia5p7iovqqtc4fhsdi9bgp41visv.apps.googleusercontent.com',
-        clientSecret: 'KT5BHLFdA3EutIoZs7nkw7T5'
-    }
-};
